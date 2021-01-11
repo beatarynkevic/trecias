1a66b1185734a619049612b8d67af84130ae578e
Beata <beata.rynkevic1@gmail.com>
2021-01-11 15:54:21 +0200

    initial code

--- src/trecias/Trecias.java
+++ src/trecias/Trecias.java
@@ -15,7 +15,27 @@ public class Trecias {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int i = -2;
+        int j = 10;
+
+//        if (i > 0 && j++ > 100) {
+//            System.out.println("oj");
+//        }
+//        System.out.println(i);
+//        System.out.println(j);
+
+        if (i++ < 0 || j++ < 0){
+            System.out.println("kazkuris neigiamas");
+        }
+
+
+//        if(i > -10 && i < 0){
+//            System.out.println("neigiamas vienzenklis");
+//        } else if(i >= 0 && i < 10) {
+//                System.out.println("vienzenklis");
+//        } else {
+//            System.out.println("per didelis");
+//        }
     }
-    
+
 }
